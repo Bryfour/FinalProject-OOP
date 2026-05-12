e8b38f6ff1180b78da8502dd5df03e9b4c718226
Bryfour <Bryancierniak@gmail.com>
2026-05-11 22:58:02 -0400

    added the star interface and comment

---
+++ src/Final_ProjectPkg/Star.java
@@ -0,0 +1,10 @@
+package Final_ProjectPkg;
+
+public interface Star {
+}
+
+
+// just beginning for this interface.
+// drag and drop the star from the ui,
+// may or may not need to be moved out of the frame,
+// not sure if the star is supposed to appear and dissappear
